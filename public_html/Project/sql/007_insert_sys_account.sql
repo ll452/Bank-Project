235b43763256c60c6944d5145819b298d2421a4f
ll452 <ll452@njit.edu>
2022-04-07 19:53:53 -0400

    changes in insert account sql file

--- public_html/Project/sql/007_insert_sys_account.sql
+++ public_html/Project/sql/007_insert_sys_account.sql
@@ -1,2 +1,2 @@
 INSERT INTO `Users` (id, email, password) VALUES (-1, "system.user@simplebank.com", "laple1019");
-INSERT INTO `Accounts` (account_number, user_id, account_type) VALUES ("000000000000", -1, "World");
+INSERT INTO `Accounts` (id, account_number, user_id, account_type) VALUES (-1, "000000000000", -1, "World");
