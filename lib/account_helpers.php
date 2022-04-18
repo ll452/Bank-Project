39cc36a363f41cad9a4ca645b89b2d83b2fad6a0
ll452 <ll452@njit.edu>
2022-04-17 20:32:33 -0400

    adding withdraw and deposit features with transactions history

--- lib/account_helpers.php
+++ lib/account_helpers.php
@@ -1,15 +1,18 @@
 <?php
-function get_account_balance()
+function get_user_account_balance($user_id)
 {
-    if (is_logged_in() && isset($_SESSION["user"]["account"])) {
-        return (int)se($_SESSION["user"]["account"], "balance", 0, false);
-    }
-    return 0;
-}
-function get_user_account_id()
-{
-    if (is_logged_in() && isset($_SESSION["user"]["account"])) {
-        return (int)se($_SESSION["user"]["account"], "id", 0, false);
+    $query = "SELECT balance from Accounts WHERE user_id = :id";
+    $db = getDB();
+    $stmt = $db->prepare($query);
+    try {
+        $stmt->execute([":id" => $user_id]);
+        $r = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($r) {
+            return (int)se($r, "balance", 0, false);
+        }
+    } catch (PDOException $e) {
+        error_log("Error fetching balance for user: " . var_export($e->errorInfo, true));
+        flash("Error looking up balance", "danger");
     }
     return 0;
 }
--- public_html/Project/list_accounts.php
+++ public_html/Project/list_accounts.php
@@ -1,8 +1,8 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
-
+is_logged_in(true);
 $user_id = get_user_id();
-$query = "SELECT account_number, account_type, modified, balance from Accounts WHERE user_id = $user_id ORDER BY modified desc LIMIT 5";
+$query = "SELECT id, account_number, account_type, modified, balance from Accounts WHERE user_id = $user_id ORDER BY modified desc LIMIT 5";
 $db = getDB();
 $params = null;
 $stmt = $db->prepare($query);
@@ -12,6 +12,8 @@ try {
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     if ($results) {
         $accounts = $results;
+        
+
     } else {
         flash("No Accounts found", "warning");
     }
@@ -29,25 +31,24 @@ try {
             <th>Modified</th>
             <th>Balance</th>
         </thead>
-        <?php if (empty($accounts)) : ?>
+        <tbody>
+            <?php if (empty($accounts)) : ?>
                 <tr>
                     <td colspan="100%">No Accounts</td>
                 </tr>
             <?php else : ?>
                 <?php foreach ($accounts as $account) : ?>
                     <tr>
-                        <td><?php se($account, "account_number"); ?></td>
+                        <td><a href="transaction_history.php?id=<?php echo se($account, "id");?>&acc_num=<?php echo se($account, "account_number");?>"> <?php se($account, "account_number"); ?></a></td>
                         <td><?php se($account, "account_type"); ?></td>
                         <td><?php se($account, "modified"); ?></td>
                         <td><?php se($account, "balance"); ?></td>
                     </tr>
                 <?php endforeach; ?>
             <?php endif; ?>
-        <tbody>
-
         </tbody>
     </table>
-
+</div>
 
 <?php
 require_once(__DIR__ . "/../../partials/flash.php");
